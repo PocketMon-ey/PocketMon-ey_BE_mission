da4045a34881f7fa7cb496c3bb50ab1eb54e9f3a
k03147 <ek03147@gmail.com>
2023-12-22 10:56:32 +0900

    🚑Contructor 초기화 수정

--- src/main/java/com/pocketmoney/mission/model/StatusParam.java
+++ src/main/java/com/pocketmoney/mission/model/StatusParam.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class StatusParam {
-    public StatusParam(int id2, int i) {
+    public StatusParam(int id, int status) {
+        this.id = id;
+        this.status = status;
     }
     private int id;
     private int status;
